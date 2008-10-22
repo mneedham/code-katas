e7821fc4c9c725c993e0592a87da16500f3d0d1b
Mark Needham <m.h.needham@gmail.com>
2008-10-23 09:20:15 +1100

    Ignoring some files

--- CodeKata1/src/test/java/pricing/strategies/ThreeForADollarTest.java
+++ CodeKata1/src/test/java/pricing/strategies/ThreeForADollarTest.java
@@ -3,6 +3,7 @@ package pricing.strategies;
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import org.mvel2.MVEL;
 import static pricing.strategies.PriceUtils.with;
 import pricing.Price;
 
@@ -16,14 +17,14 @@ public class ThreeForADollarTest {
         assertThat(price, equalTo(price("1")));
     }
 
-   @Test
-   public void shouldApplyDiscountOnlyForThreePrices() {
-       ThreeForADollar threeForADollar = new ThreeForADollar();
+    @Test
+    public void shouldApplyDiscountOnlyForThreePrices() {
+        ThreeForADollar threeForADollar = new ThreeForADollar();
 
-       Price price = threeForADollar.calculateTotalPrice(with(price("0.50"), price("0.50"), price("0.50"), price("0.50")));
+        Price price = threeForADollar.calculateTotalPrice(with(price("0.50"), price("0.50"), price("0.50"), price("0.50")));
 
-       assertThat(price, equalTo(price("1.50")));
-   }
+        assertThat(price, equalTo(price("1.50")));
+    }
 
     private Price price(String value) {
         return new Price(new Double(value));
